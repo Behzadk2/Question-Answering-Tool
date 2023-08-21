48f37dcd3b48cd574fb81df25f81a0995a199dd1
behzad kazemi <bkazemi1994@gmail.com>
2023-08-21 22:09:52 +0200

    final change before added css

--- src/QuestionAnsweringTool.jsx
+++ src/QuestionAnsweringTool.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+
+const fixedText = "Xylitol is also known as birch sugar. Xylitol tastes just as sweet as sugar, and you can replace it one-to-one in recipes. But why should you do that? Birch sugar has some positive advantages over table sugar. On the one hand, it is tooth-friendly and even has a non-cariogenic effect, i.e. it does not cause caries. Secondly, xylitol is particularly popular because of its low glycemic index. It has a glycemic index of 7 to 11, while that of regular sugar is 65 to 100. So if you want to consciously avoid the negative properties of sugar, but not its sweetness, you should give xylitol a try. However, in moderation, because in high doses it can have a laxative effect. Birch sugar should also be kept away from dogs, as it can even be fatal for the four-legged friends.";
+const fixedQuestion = "What is the advantage of birch sugar?";
 
 const QuestionAnsweringTool = () => {
   const [inputText, setInputText] = useState('');
@@ -8,14 +12,6 @@ const QuestionAnsweringTool = () => {
   const [answer, setAnswer] = useState('');
   const [contextBefore, setContextBefore] = useState('');
   const [contextAfter, setContextAfter] = useState('');
-
-  const handleInputChange = (event) => {
-    setInputText(event.target.value);
-  };
-
-  const handleQuestionChange = (event) => {
-    setQuestion(event.target.value);
-  };
 
   const handleAnswer = async () => {
     try {
@@ -45,56 +41,45 @@ const QuestionAnsweringTool = () => {
     }
   };
 
+  const handleLoadFixedTextAndQuestion = () => {
+    setInputText(fixedText);
+    setQuestion(fixedQuestion);
+  };
+
   return (
-    <div
-      style={{
-        background: '#fafafa',
-        padding: '20px',
-      }}
-    >
-      <div
-        style={{
-          background: '#ffffff',
-          padding: '10px',
-          borderRadius: '5px',
-          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-          animation: 'fadein 0.5s',
-        }}
-      >
-        <h1 class="text-center mb-4">Question Answering Tool</h1>
-        <div style={{ margin: '10px 0' }}>
+    <div className="bg-lightgray p-5">
+      <div className="container bg-white p-4 rounded">
+        <h1 className="text-center mb-4">Question Answering Tool</h1>
+        <div className="mb-3">
           <textarea
-            class="form-control"
+            className="form-control"
             placeholder="Enter the input text..."
             rows="6"
             value={inputText}
-            onChange={handleInputChange}
+            onChange={(e) => setInputText(e.target.value)}
           />
         </div>
-        <div style={{ margin: '10px 0' }}>
+        <div className="mb-3">
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             placeholder="Enter your question..."
             value={question}
-            onChange={handleQuestionChange}
+            onChange={(e) => setQuestion(e.target.value)}
           />
-          <button
-            class="btn btn-primary mt-2"
-            style={{ color: '#ffffff', background: '#337ab7' }}
-            onClick={handleAnswer}
-          >
+          <button className="btn btn-primary mt-2" onClick={handleAnswer}>
             Get Answer
+          </button>
+          <button className="btn btn-secondary mt-2 ml-2" onClick={handleLoadFixedTextAndQuestion}>
+            Load Fixed Text & Question
           </button>
         </div>
         {answer && (
-          <div style={{ margin: '10px 0' }}>
+          <div className="mb-3">
             <h2>Answer:</h2>
-            <p class="bg-lightgreen p-2 rounded animate__animated animate__fadeInUp">
+            <p className="bg-lightgreen p-2 rounded">
               {contextBefore}
-              <span class="bg-success text-white p-1 rounded">
-                {answer}
-              </span>
+              <span className="bg-success text-white p-1 rounded">{answer}</span>
               {contextAfter}
             </p>
           </div>
